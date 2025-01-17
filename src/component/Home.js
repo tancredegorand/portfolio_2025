d450c5c24e859f8ef1d75cf51bcbd9ee0f573111
tancredegorand <119679491+tancredegorand@users.noreply.github.com>
2025-01-17 00:47:56 -0500

    home

---
+++ src/component/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div class="home">
+        <div>
+            <h1>Tancrède Gorand</h1>
+            <p class="subtitle">INGÉNIERIE CRÉATIVE</p>
+        </div>
+ 
+    </div>
+  );
+};
+
+export default Home;
