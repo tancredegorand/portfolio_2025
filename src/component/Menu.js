df72d97e3d69e83f06aafa36323baf4e94422f58
tancredegorand <119679491+tancredegorand@users.noreply.github.com>
2025-01-29 11:35:42 -0500

    menu animation

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,11 +1,22 @@
 import { useEffect, useState } from 'react';
 
+
+
+
+
+
+
+
+
 const Menu = () => {
     const [isOpen, setIsOpen] = useState(false);
   
     useEffect(() => {
       const burger = document.getElementById('burger');
       const spans = burger.getElementsByTagName('span');
+      const menu_container = document.getElementById('menu_container'); 
+      const menu_list = document.getElementById('menu_list'); 
+      const body = document.body;
       
       const handleBurgerClick = (e) => {
         e.preventDefault();
@@ -14,9 +25,15 @@ const Menu = () => {
         if (!isOpen) {
           spans[0].classList.add('isopen1');
           spans[1].classList.add('isopen2');
+          menu_container.classList.add('menu_hide');
+          menu_list.classList.add('menu_liste_show');
+          body.style.overflow = 'hidden';
         } else {
           spans[0].classList.remove('isopen1');
           spans[1].classList.remove('isopen2');
+          menu_container.classList.remove('menu_hide');
+          menu_list.classList.remove('menu_liste_show');
+          body.style.overflow = 'auto';
         }
       };
   
@@ -28,24 +45,31 @@ const Menu = () => {
     }, [isOpen]);
   
     return (
-      <div className="menu">
         <div>
-          <a href="#accueil">Accueil</a>
+            <div className="menu menu_hide" id='menu_container'>
+                <div>
+                    <a href="#accueil">Accueil</a>
+                </div>
+                <div className="burger" id="burger">
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <div className='menu_list' id='menu_list'>
+                <ul>
+                    <li>Accueil</li>
+                    <li>Développement</li>
+                    <li>Son</li>
+                    <li>Visuel</li>
+                </ul>
+            </div>
         </div>
-  
-        <div className="burger" id="burger">
-          <span></span>
-          <span></span>
-        </div>
-   
-        <ul>
-          <li>Accueil</li>
-          <li>Développement</li>
-          <li>Son</li>
-          <li>Visuel</li>
-        </ul>
-      </div>
     );
   };
   
   export default Menu;
+
+
+
+
+
