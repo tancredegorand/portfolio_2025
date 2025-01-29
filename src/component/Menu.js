7c0e775689a877a2695e9b7e25350e11286a3989
tancredegorand <119679491+tancredegorand@users.noreply.github.com>
2025-01-28 23:38:15 -0500

    menu burger animation

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -1,21 +1,51 @@
-const Menu = () =>{
+import { useEffect, useState } from 'react';
+
+const Menu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    useEffect(() => {
+      const burger = document.getElementById('burger');
+      const spans = burger.getElementsByTagName('span');
+      
+      const handleBurgerClick = (e) => {
+        e.preventDefault();
+        setIsOpen(!isOpen);
+        
+        if (!isOpen) {
+          spans[0].classList.add('isopen1');
+          spans[1].classList.add('isopen2');
+        } else {
+          spans[0].classList.remove('isopen1');
+          spans[1].classList.remove('isopen2');
+        }
+      };
+  
+      burger.addEventListener('click', handleBurgerClick);
+  
+      return () => {
+        burger.removeEventListener('click', handleBurgerClick);
+      };
+    }, [isOpen]);
+  
     return (
-        <div className="menu">
-            <div><a> Accueil</a></div>
-            <div className="burger">
-                <span></span>
-                <span></span>
-            </div>
-            <ul>
-                <li>Accueil</li>
-                <li>Développement</li>
-                <li>Son</li>
-                <li>Visuel</li>
-            </ul>
+      <div className="menu">
+        <div>
+          <a href="#accueil">Accueil</a>
         </div>
+  
+        <div className="burger" id="burger">
+          <span></span>
+          <span></span>
+        </div>
+   
+        <ul>
+          <li>Accueil</li>
+          <li>Développement</li>
+          <li>Son</li>
+          <li>Visuel</li>
+        </ul>
+      </div>
     );
-};
-
-export default Menu; 
-
-
+  };
+  
+  export default Menu;
