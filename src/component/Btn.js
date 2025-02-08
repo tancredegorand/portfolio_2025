aee51a9d3a581d8dc2d549fc31c531dd832ad701
tancredegorand <119679491+tancredegorand@users.noreply.github.com>
2025-02-08 00:57:54 -0500

    projects

--- src/component/Btn.js
+++ src/component/Btn.js
@@ -1,16 +1,18 @@
 import arrow_link from '../svg/arrow_link.svg'
 
 
-const Btn = ({text}) => {
+const Btn = ({text, url}) => {
     return(
-        <button>
-        <div>
-            <span></span>
-            <span id='span-btn-not-fixed'></span>
-        </div>
-        <p>{text}</p>
-        <img src={arrow_link}/>
-    </button>
+        <a href={url} target='_blank'>
+            <button>
+                <div>
+                    <span></span>
+                    <span id='span-btn-not-fixed'></span>
+                </div>
+                <p>{text}</p>
+                <img src={arrow_link}/>
+            </button>
+        </a>
     ); 
 }; 
 
--- src/component/Project.js
+++ src/component/Project.js
@@ -5,16 +5,27 @@ const Project = ({project, titreSection}) => {
         <div className='project-card' id={project.id}>
         <div>
             <div className='project-title'>
-                <p>{titreSection}</p>
                 <h2>{project.titre_projet}</h2>
+                <p>{titreSection} - {project.date}</p>
             </div>
             <p className='project-card-text'>{project.description}</p>
         </div>
-        <Btn text="VOIR"/>
+        {project.url && <Btn text="VOIR" url={project.url} />}
         <div className='project-img'>
-            {project.images.map((img, index) => (
+        {project.images && project.images.length > 0 && (
+            project.images.map((img, index) => (
                 <img key={index} src={`/img/${img}`} alt={`Project ${index}`} />
-            ))}
+            ))
+        )}
+
+        {project.videos && project.videos.length > 0 && (
+            project.videos.map((video, index) => (
+                <video autoPlay loop muted playsInline key={index}>
+                    <source src={`/video/${video}`} type="video/mp4" />
+                </video>
+            ))
+        )}
+
         </div>
         </div>
 
